d106b86747bae89bdbdd905ece368f3a4d567429
ConnorWeese <71419035+ConnorWeese@users.noreply.github.com>
2020-11-15 22:40:05 -0700

    Added button controls in page 2

--- FGOTool/Form1.cs
+++ FGOTool/Form1.cs
@@ -19,6 +19,14 @@ namespace FGOTool
         User user;
         //create a global servant so we can use it's data throughout the app
         Servant servantToAdd;
+        //create a global list of textboxes to be used on the "My Servant" tab
+        List<TextBox> servantMaterialTextBoxes;
+        //create a global list of buttons to be used on the "My Servant" tab
+        List<Button> servantMaterialIncrementButtons;
+        //create a global list of buttons to be used on the "My Servant" tab
+        List<Button> servantMaterialDecrementButtons;
+        //create a global list of strings to be used to know what material buttons are refering to
+        List<String> servantMaterials;
 
         public Form1()
         {
@@ -129,12 +137,151 @@ namespace FGOTool
             splitContainer1.Panel2.Controls.Clear();    //clear everything from panel 2
 
             Servant servant = user.getServant((sender as Button).Text);    //get the servant to pull data from
+            servantToAdd = servant;
 
             displayServantData(servant);
         }
 
+        //event handler for when a user clicks on the increment button for a material in the "My Servants" tab
+        private void servantMaterialIncrementButton_ClickEvent(Object sender, EventArgs e, int index, String servantName)
+        {
+            int materialNum; 
+            bool success = Int32.TryParse(servantMaterialTextBoxes[index].Text, out materialNum);   //tryparse the textbox to make sure it's an integer
+            if (success)
+            {
+                materialNum++;  //increment the material number
+
+                //if the material number is less than the max for a servant
+                if(materialNum < user.getServant(servantName).getTotalMats()[servantMaterials[index]])
+                {
+                    servantMaterialTextBoxes[index].Text = materialNum.ToString();  //set the textbox's text to the new value
+                }
+                //if the material number is equal to the max
+                else if(materialNum == user.getServant(servantName).getTotalMats()[servantMaterials[index]])
+                {
+                    servantMaterialTextBoxes[index].Text = materialNum.ToString();  //set the textbox's text
+                    (sender as Button).Enabled = false; //disable the increment button
+                }
+                //if the material number is greater than the max
+                else
+                {
+                    servantMaterialTextBoxes[index].Text = user.getServant(servantName).getTotalMats()[servantMaterials[index]].ToString(); //set the textbox's text to the servant's max
+                    (sender as Button).Enabled = false; //disable the increment button
+                }
+
+                //enable the decrement button
+                servantMaterialDecrementButtons[index].Enabled = true;
+            }
+            else
+            {
+                //if the textbox is empty
+                if(servantMaterialTextBoxes[index].Text == "")
+                {
+                    servantMaterialTextBoxes[index].Text = "1"; //set the textbox's text to 1
+
+                    //enable the decrement button
+                    servantMaterialDecrementButtons[index].Enabled = true;
+                }
+            }
+        }
+
+        //event handler for when a user clicks on the decrement button for a material in the "My Servant" tab
+        private void servantMaterialDecrementButton_ClickEvent(Object sender, EventArgs e, int index, String servantName)
+        {
+            int materialNum;
+            bool success = Int32.TryParse(servantMaterialTextBoxes[index].Text, out materialNum);
+            if (success)
+            {
+                materialNum--;  //decrement the material number
+
+                //if the material number is greater than 0
+                if(materialNum > 0)
+                {
+                    servantMaterialTextBoxes[index].Text = materialNum.ToString();  //set the textbox's text to the new value
+                }
+                //if the material number is equal to 0
+                else if(materialNum == 0)
+                {
+                    servantMaterialTextBoxes[index].Text = materialNum.ToString();  //set the textbox's text
+                    (sender as Button).Enabled = false; //disable the decrement button
+                }
+                //if the material number is less than 0
+                else
+                {
+                    servantMaterialTextBoxes[index].Text = "0";  //set the textbox's text to 0
+                    (sender as Button).Enabled = false; //disable the decrement button
+                }
+
+                //enable the increment button
+                servantMaterialIncrementButtons[index].Enabled = true;
+            }
+            else
+            {
+
+            }
+        }
+
+        //event handler for when a user types in the material textbox on the "My Servant" tab
+        private void servantMaterialTextBox_KeyPress(Object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if(e.KeyChar == '.')
+            {
+                e.Handled = true;
+            }
+        }
+
+        //event handler for when a user types in or pasts in the material textbox on the "My Servant" tab
+        private void servantMaterialTextBox_TextChanged(Object sender, EventArgs e, int index, String servantName)
+        {
+            int materialNum;
+            bool success = Int32.TryParse((sender as TextBox).Text, out materialNum);
+            if (success)
+            {
+                if(materialNum > user.getServant(servantName).getTotalMats()[servantMaterials[index]])
+                {
+                    (sender as TextBox).Text = user.getServant(servantName).getMatCount()[servantMaterials[index]].ToString();
+                }
+                else if(materialNum < 0)
+                {
+                    (sender as TextBox).Text = user.getServant(servantName).getMatCount()[servantMaterials[index]].ToString();
+                }
+                else
+                {
+                    //(sender as TextBox).Text = materialNum.ToString();
+                }
+            }
+            else
+            {
+                if((sender as TextBox).Text != "")
+                {
+                    (sender as TextBox).Text = user.getServant(servantName).getMatCount()[servantMaterials[index]].ToString();
+                }
+            }
+        }
+
+        //event handler to highlight all text in a textbox when selected
+        private void servantMaterialTextBox_Selected(Object sender, EventArgs e)
+        {
+            (sender as TextBox).SelectAll();
+            (sender as TextBox).Focus();
+        }
+
+        //private method to display a Servant's data on panel 2 in the "My Servant" tab
         private void displayServantData(Servant servant)
         {
+            //set servantMaterialTextBoxes to a new List
+            servantMaterialTextBoxes = new List<TextBox>();
+            //set servantMaterialIncrementButtons to a new List
+            servantMaterialIncrementButtons = new List<Button>();
+            //set servantMaterialDecrementButtons to a new List
+            servantMaterialDecrementButtons = new List<Button>();
+            //set servantMaterials to a new List
+            servantMaterials = new List<String>();
+
             Label serName = new Label();    //create a label for the servants name
             serName.AutoSize = true;    //set label autosize to true
             serName.Font = new Font(serName.Font.FontFamily, serName.Font.Size + 10.0f, serName.Font.Style);    //increase the label's font size
@@ -202,8 +349,6 @@ namespace FGOTool
                 serMatNames.Location = new Point(splitContainer1.Location.X + serName.Height + 10, splitContainer1.Location.Y + serHPNames.Height + 65 + (35 * counter));
                 serMatNames.Text += mat.Key + ":";
 
-                
-
                 //label to display the total number of a material that a servant needs
                 Label serMatNums = new Label();
                 serMatNums.Width = 110;
@@ -218,17 +363,74 @@ namespace FGOTool
                 //      or remember how much they've gone up by an manually type it in
                 if (counter != 0)
                 {
+                    //get the current value of counter to use for the increment and decrement button events
+                    int index = counter;
+
+                    //add the material to the servantMaterials List
+                    servantMaterials.Add(mat.Key);
+
                     //textbox to display user entered values for the number of materials used
                     TextBox usedMatBox = new TextBox();
                     usedMatBox.Font = new Font(usedMatBox.Font.FontFamily, usedMatBox.Font.Size + 5.0f, usedMatBox.Font.Style);
                     usedMatBox.Location = new Point(splitContainer1.Location.X + serMatNums.Location.X + serMatNums.Width + 40, splitContainer1.Location.Y + serHPNames.Height + 63 + (35 * counter));
                     usedMatBox.TextAlign = HorizontalAlignment.Right;
                     usedMatBox.Text = String.Format("{0:n0}", servant.getMatCount()[mat.Key]);
-                    
+                    //add an event to limit textbox character input
+                    usedMatBox.KeyPress += servantMaterialTextBox_KeyPress;
+                    //add an event to check what text gets put in the textbox
+                    usedMatBox.TextChanged += (sender, EventArgs) => { servantMaterialTextBox_TextChanged(sender, EventArgs, index - 1, servant.getName()); };
+                    //add an event to highlight all the text in the textbox when selected
+                    usedMatBox.Click += servantMaterialTextBox_Selected;
+
+                    Button decrement = new Button();
+                    decrement.Width = usedMatBox.Height;
+                    decrement.Height = usedMatBox.Height;
+                    decrement.Location = new Point(splitContainer1.Location.X + usedMatBox.Location.X + usedMatBox.Width + 10, splitContainer1.Location.Y + serHPNames.Height + 63 + (35 * counter));
+                    decrement.Font = new Font(decrement.Font.FontFamily, decrement.Font.Size + 3.0f, decrement.Font.Style);
+                    decrement.TextAlign = ContentAlignment.MiddleCenter;
+                    decrement.Text = "-";
+                    //add an event to the decrement button click, pass in counter-1 so we have the index in the List where the button will be stored
+                    decrement.Click += (sender, EventArgs) => { servantMaterialDecrementButton_ClickEvent(sender, EventArgs, index - 1, servant.getName()); };
+
+                    //if the user entered material number is less than or equal to zero
+                    if (servant.getMatCount()[mat.Key] <= 0)
+                    {
+                        //disable the decrement button
+                        decrement.Enabled = false;
+                    }
+
+                    Button increment = new Button();
+                    increment.Width = usedMatBox.Height;
+                    increment.Height = usedMatBox.Height;
+                    increment.Location = new Point(splitContainer1.Location.X + decrement.Location.X + decrement.Width + 5, splitContainer1.Location.Y + serHPNames.Height + 63 + (35 * counter));
+                    increment.Font = new Font(increment.Font.FontFamily, increment.Font.Size + 3.0f, increment.Font.Style);
+                    increment.TextAlign = ContentAlignment.MiddleCenter;
+                    increment.Text = "+";
+                    //add an event to the increment button click, pass in counter-1 so we have the index in the List where the button will be stored
+                    increment.Click += (sender, EventArgs) => { servantMaterialIncrementButton_ClickEvent(sender, EventArgs, index - 1, servant.getName()); };
+
+                    //if the user entered material number is greater than or equal to the max for this servant
+                    if(servant.getMatCount()[mat.Key] >= mat.Value)
+                    {
+                        //disable the increment button
+                        increment.Enabled = false;
+                    }
+
                     //add the textbox to the panel controls
                     splitContainer1.Panel2.Controls.Add(usedMatBox);
+                    //add the button to the panel controls
+                    splitContainer1.Panel2.Controls.Add(decrement);
+                    //add the button to the panel controls
+                    splitContainer1.Panel2.Controls.Add(increment);
+
+                    //add the textbox to the global textbox list
+                    servantMaterialTextBoxes.Add(usedMatBox);
+                    //add the button to the global decrement button list
+                    servantMaterialDecrementButtons.Add(decrement);
+                    //add the button to the global increment button list
+                    servantMaterialIncrementButtons.Add(increment);
                 }
-                
+
                 //increment the counter
                 counter++;
 
